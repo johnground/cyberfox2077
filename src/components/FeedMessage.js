b12eb4c6a8e1d3b60b8d9f8e6a4efee397669b9c
lonewolf <lonewolf@silent.storm>
2023-12-31 13:30:12 -0500

    modified FeedMessage.js and MessagePage.js to have the beginings of terminal like functionality

--- src/components/FeedMessage.js
+++ src/components/FeedMessage.js
@@ -1,7 +1,19 @@
 import { h } from 'preact';
 
 const FeedMessage = ({ message }) => {
-  return <div>{message}</div>;
+  if (typeof message === 'string') {
+    return <div className="message">{message}</div>;
+  } else if (typeof message === 'object' && message.command && message.commandOutput) {
+    return (
+      <div className="message">
+        <div className="message__body">{message.commandOutput}</div>
+        <div className="message__footer">{message.command}</div>
+      </div>
+    );
+  } else {
+    return <div className="message">Unknown message format</div>;
+  }
 };
 
 export default FeedMessage;
+
--- src/components/MessagePage.js
+++ src/components/MessagePage.js
@@ -2,11 +2,84 @@ import { h, Component } from 'preact';
 import FeedMessage from './FeedMessage';
 
 class MessagePage extends Component {
+  // Step 1
+  state = {
+    command: '',
+    output: [],
+  };
+
+  // Step 2
+  handleCommandChange = (event) => {
+    this.setState({ command: event.target.value });
+  };
+
+  // Step 3
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const { command, output } = this.state;
+
+    if (!command.trim()) {
+      return; // If command is only whitespace, do nothing
+    }
+
+    let commandOutput;
+    // Simulate command processing
+    switch (command.trim()) {
+      case 'help':
+        commandOutput = 'Available commands: help, list, exit';
+        break;
+      // Add other cases for different commands here
+      default:
+        commandOutput = `Command not recognized: ${command}`;
+        break;
+    }
+
+    // Append to output
+    this.setState({
+      output: [...output, { command, commandOutput }],
+      command: '', // Clear the input field
+    });
+  };
+
+  // Step 4
   render() {
+    const { command, output } = this.state;
     return (
-      <div>
-        <FeedMessage message="Test Message" />
-        {/* Additional content for MessagePage */}
+      <div className="channel-feed">
+        <div className="channel-feed__body">
+          {/* Display each command and its output */}
+          {output.map((entry, index) => (
+            <FeedMessage key={index} message={entry.commandOutput} />
+          ))}
+        </div>
+        <div className="channel-feed__footer">
+          <form
+            className="channel-message-form"
+            onSubmit={this.handleSubmit}
+          >
+            <div className="form-group">
+              <label htmlFor="command" className="form-label">Command</label>
+              <div className="form-control">
+                <textarea
+                  id="command"
+                  className="form-control"
+                  name="command"
+                  value={command}
+                  onInput={this.handleCommandChange}
+                  placeholder="Type your command..."
+                ></textarea>
+              </div>
+            </div>
+            <div className="form-footer">
+              <button
+                className="button button--primary button--size-xl"
+                type="submit"
+              >
+                Execute
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
