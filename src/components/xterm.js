7f54a57f634cd1c3262e800ae847a7a8cfc24438
lonewolf <lonewolf@silent.storm>
2024-01-19 09:02:05 -0500

    Added module6 reamde and worked on terminal styling

--- src/components/xterm.js
+++ src/components/xterm.js
@@ -4,14 +4,15 @@ import '/styles/styles.css';
 import '/styles/xterm.css';
 import io from 'socket.io-client';
 import { FitAddon } from 'xterm-addon-fit';
-import Resizable from './Resizable'; // Ensure this is the correct path to your Resizable component
-
 
 class TerminalComponent extends Component {
     constructor(props) {
         super(props);
         this.state = { minimized: false, height: 250 };
         this.terminalRef = null;
+        this.resizeHandleRef = null;
+        this.fitAddon = new FitAddon();
+        this.socket = null;
     }
 
     toggleMinimize = () => {
@@ -24,17 +25,18 @@ class TerminalComponent extends Component {
 
     componentDidMount() {
         this.terminal = new Terminal({ cursorBlink: true });
-        this.fitAddon = new FitAddon();
         this.terminal.loadAddon(this.fitAddon);
         this.terminal.open(this.terminalRef);
         this.fitAddon.fit();
         this.setupSocket();
+        this.initializeResizeHandle();
     }
 
     setupSocket() {
         this.socket = io('http://localhost:3000');
         this.socket.on('terminal-output', data => {
             this.terminal.write(data);
+            this.scrollToBottom(); // Call this after writing data
         });
         this.terminal.onData(data => {
             this.socket.emit('terminal-input', data);
@@ -50,57 +52,45 @@ class TerminalComponent extends Component {
         }
     }
 
-    handleResize = (newHeight) => {
-        this.setState({ height: newHeight }, () => {
+    handleResize = (e) => {
+        const terminalBottom = this.terminalRef.getBoundingClientRect().bottom;
+        const newHeight = terminalBottom - e.clientY;
+        this.setState({ height: Math.max(newHeight, 50) }, () => {
             this.fitAddon.fit();
         });
     };
 
-
-    setupSocket() {
-        this.socket = io('http://localhost:3000');
-        this.socket.on('terminal-output', data => {
-            this.terminal.write(data);
-        });
-        this.terminal.onData(data => {
-            this.socket.emit('terminal-input', data);
-        });
-    }
-
-    componentWillUnmount() {
-        if (this.socket) {
-            this.socket.disconnect();
+    scrollToBottom = () => {
+        const viewport = this.terminalRef.querySelector('.xterm-viewport');
+        if (viewport) {
+            viewport.scrollTop = viewport.scrollHeight;
         }
-        if (this.terminal) {
-            this.terminal.dispose();
-        }
-    }
-
-    // Method to initialize the resize handle functionality
-    initializeResizeHandle = () => {
-        let startHeight, startY;
-
-        const doDrag = (e) => {
-            const newHeight = startHeight + (e.clientY - startY);
-            this.setState({ height: Math.max(this.props.minHeight, newHeight) }, () => {
-                this.fitAddon.fit();
-            });
-        };
-
-        const stopDrag = () => {
-            document.documentElement.removeEventListener('mousemove', doDrag);
-            document.documentElement.removeEventListener('mouseup', stopDrag);
-        };
-
-        const startDrag = (e) => {
-            startY = e.clientY;
-            startHeight = this.state.height;
-            document.documentElement.addEventListener('mousemove', doDrag);
-            document.documentElement.addEventListener('mouseup', stopDrag);
-        };
-
-        this.resizeHandleRef.addEventListener('mousedown', startDrag);
     };
+
+    initializeResizeHandle = () => {
+        let initialY = null;
+
+        const onMouseMove = (e) => {
+            if (initialY !== null) {
+                this.handleResize(e);
+            }
+        };
+
+        const onMouseUp = () => {
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', onMouseUp);
+            initialY = null;
+        };
+
+        const onMouseDown = (e) => {
+            initialY = e.clientY;
+            document.addEventListener('mousemove', onMouseMove);
+            document.addEventListener('mouseup', onMouseUp);
+        };
+
+        this.resizeHandleRef.addEventListener('mousedown', onMouseDown);
+    };
+    
     render({ }, { minimized, height }) {
         const buttonSymbol = minimized ? '⤢' : '–';
         return (
@@ -108,15 +98,13 @@ class TerminalComponent extends Component {
                 <button onClick={this.toggleMinimize} className="terminal__minimize-btn">
                     {buttonSymbol}
                 </button>
+                <div className="resize-handle" ref={el => (this.resizeHandleRef = el)} />
                 <div ref={el => (this.terminalRef = el)} className="terminal__wrapper">
-                    {/* Terminal content will be rendered here */}
+                    {/* Terminal content goes here */}
                 </div>
-                <Resizable height={height} onResize={this.handleResize} minHeight={50} />
             </div>
         );
     }
 }
 
-
 export default TerminalComponent;
-
