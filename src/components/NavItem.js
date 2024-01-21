0cff8f5fa4707049ae7c0189d2c8069539977a2d
lonewolf <lonewolf@silent.storm>
2024-01-21 18:46:25 -0500

    Fixed CYBERFOX-2077 HOME, shortcut link back to the home page

--- src/components/NavItem.js
+++ src/components/NavItem.js
@@ -2,17 +2,30 @@ import { h } from 'preact';
 import Badge from './Badge'; // Assuming Badge is also modularized
 
 export default function NavItem({ navItem }) {
+
+  const handleNavigation = (event) => {
+    // Prevent default anchor behavior
+    event.preventDefault();
+
+    // Navigate to the root path if the link is "CyberFox-2077 Home"
+    if (navItem.text === "CyberFox-2077 Home") {
+      window.location.href = "http://cyberfox:3000/";
+    } else {
+      // You can extend this condition to handle other specific links
+      // For now, do nothing or handle other cases as needed
+    }
+  };
+
   return (
     <li className="nav__item">
       <a
         className={`nav__link ${navItem.isActive ? "nav__link--active" : ""}`}
         href="#"
+        onClick={handleNavigation} // Add the onClick event listener here
       >
         <span className="nav__link__element">{navItem.text}</span>
         {navItem.notificationCount > 0 && (
-          <span className="nav__link__element">
-            <Badge>{navItem.notificationCount}</Badge>
-          </span>
+          <Badge>{navItem.notificationCount}</Badge>
         )}
       </a>
     </li>
